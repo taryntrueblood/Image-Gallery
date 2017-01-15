a99c954f37db13bf59fb5366f79f52139127decb
Taryn Trueblood <taryntrueblood@yahoo.com>
2017-01-14 21:22:18 -0600

    Css and javascript changes
    Made some changes in css.  Added new js files.

--- js/gallery.js
+++ js/gallery.js
@@ -8,7 +8,22 @@ $('.container').magnificPopup({
     gallery:{
         enabled: true
     },
-    titleSrc: 'title',
-    
+    titleSrc: 'title'
+   
+});
+
+
+
+$("#filter").keyup(function () {
+    var filter = $(this).val(), count = 0;
+    $(".filtered:first li").each(function () {
+        if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+            $(this).addClass("hidden");
+        } else {
+            $(this).removeClass("hidden");
+            count++;
+        }
+    });
+    $("#filter-count").text(count);
 });
 
