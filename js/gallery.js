8a75abc890bff8f259c8e56ebf17159bd7565bfb
Taryn Trueblood <taryntrueblood@yahoo.com>
2017-01-17 11:59:16 -0600

    Major Changes
    I changed the plugin for the lightbox, I found a plugin for the search
    box, and corrections in the index, gallery.css, and gallery.js file

--- js/gallery.js
+++ js/gallery.js
@@ -1,29 +1,9 @@
-$(document).ready(function() {
-  $('.image-link').magnificPopup({type:'image'});
-});
 
-$('.container').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    gallery:{
-        enabled: true
-    },
-    titleSrc: 'title'
-   
-});
-
-
-
-$("#filter").keyup(function () {
-    var filter = $(this).val(), count = 0;
-    $(".filtered:first li").each(function () {
-        if ($(this).text().search(new RegExp(filter, "i")) < 0) {
-            $(this).addClass("hidden");
-        } else {
-            $(this).removeClass("hidden");
-            count++;
-        }
+ $(document).ready(function() {
+        $(".list").lightGallery();
     });
-    $("#filter-count").text(count);
-});
-
+$(document).ready(function() {
+        $("#search").hideseek({
+            attribute: 'data-alt'
+        });
+    });
